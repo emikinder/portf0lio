a6c7426397011d2ed74114aa9acc0ced0b8544ba
Emiliano <emikinderk@gmail.com>
2024-11-13 17:09:37 +0100

    new-setup

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				orange: {
-					500: '#f39c19',
-					600: '#b57008',
-				},
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        customBlack: '#161616',
+        customWhite: '#EBEBEB',
+        customPink: '#F2B5A7',
+        customGreen: '#C1D96A',
+        customPurple: '#B599F2',
+        customYellow: '#F2DD72',
+      },
+    },
+  },
+  plugins: [],
+};
