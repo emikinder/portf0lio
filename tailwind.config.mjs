4610d4d683e918a9d203a677d5720889d037d6a0
Emiliano <emikinderk@gmail.com>
2024-11-20 23:43:55 +0100

    no-ticket/marquee

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,6 +11,20 @@ export default {
         customPurple: '#B599F2',
         customYellow: '#F2DD72',
       },
+      animation: {
+        marquee: 'marquee 20s linear infinite',
+        marquee2: 'marquee2 20s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
     },
   },
   plugins: [],
