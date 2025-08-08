e9ce18728cec209554914b3c939ade72ffc5a749
emikinder <emikinderk@gmail.com>
2025-08-08 18:31:13 -0300

    added world

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import glsl from 'vite-plugin-glsl';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  vite: {
+    plugins: [glsl()]
+  }
 });
